4271e4028dcad1eb2082de6ba170a94be45f6928
LAPTOP-SIELCDOB\GKDN <1779196551@qq.com>
2024-04-15 00:14:58 +0800

    first commit

--- test/SliceScanTest.m
+++ test/SliceScanTest.m
@@ -2,12 +2,14 @@
 % Created by Wang Zhuoyue at 2024/4/13
 clc,clear,close all
 %% load the test Data
+disp('DataLoading...');
 dataFile = dir(fullfile('../testData', '*.mat'));
 fnames = {dataFile.name};
 for i = 1:size(fnames, 2)
     fname = ['../testData/' strjoin(cellstr(fnames(i)))];
     load(fname);
 end
+disp('Load complete!');
 %% load the function
 addpath('../utils/');
 %% run the test function
